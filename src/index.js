41563b0fc25031aff3a528fab7d467599d505e00
Moosa Junaadh <junaadh.02@gmail.com>
2023-08-23 20:06:24 +0800

    added dedicated spotify auth button, commented js file, gets dynamic battery from host machine

--- src/index.js
+++ src/index.js
@@ -1,16 +1,59 @@
 const dateElement = document.getElementById('date');
 const timeElement = document.getElementById('time');
 
+const powerButton = document.getElementById("power");
+const pill = document.getElementById("pill");
+const statusBar = document.getElementById("status-bar");
+const dynIsland = document.getElementById("dyn-island");
+const dynIslandAOD = document.getElementById("dyn-island-aod");
+const muteButton = document.getElementById("mute");
+const dynIslandSilent = document.getElementById("dyn-island-silent");
+const controlCenter = document.getElementById("control-center");
+const signal = document.getElementById("signal");
+const dynIslandRing = document.getElementById("dyn-island-ring");
+
+const player = document.getElementById('player');
+const cover = document.getElementById('cover');
+const trackName = document.getElementById('title');
+const artistName = document.getElementById('artist');
+const elapsed = document.getElementById('elapsed');
+const length = document.getElementById('length');
+const scrubber = document.getElementById('scrubber');
+const prevButton = document.getElementById('prev');
+const playButton = document.getElementById('playpause');
+const nextButton = document.getElementById('next');
+const playIcon = document.getElementById('play');
+const pauseIcon = document.getElementById('pause');
+const spotifyBtn = document.getElementById('spotify');
+
+const batteryPer = document.getElementById('battery-percentage');
+const batteryTip = document.getElementById('battery-tip');
+const batteryIndicator = document.getElementById('battery-indicator');
+const chargingSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="white" class="absolute top-[4px] left-[14px]" viewBox="0 0 16 16">
+<path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
+</svg>`;
+
+// setup app from spotify dev dashboard
+// replace your client id here
+const clientId = '5f56ac4110084091bcd953a152dd05a7';
+
+// setup redirect uri in spotify dev dashboard
+const redirectUri = window.location.href.split('#')[0];
+const scopes = ['user-read-playback-state', 'user-read-currently-playing', 'user-modify-playback-state'];
+
+// format date
 const dateFormat = { weekday: 'long', month: 'long', day: 'numeric' };
 const currentDate = new Date().toLocaleDateString(undefined, dateFormat);
 dateElement.textContent = currentDate;
 
+// format time
 const currentTime = new Date();
 const hours = String(currentTime.getHours()).padStart(2, '0'); // Get hours in 2-digit format
 const minutes = String(currentTime.getMinutes()).padStart(2, '0'); // Get minutes in 2-digit format
 const formattedTime = `${hours}:${minutes}`;
 timeElement.textContent = formattedTime;
 
+// set an eventlistner to listen for DOMcontentfullyloaded
 document.addEventListener("DOMContentLoaded", function() {
 
     const buttons = document.querySelectorAll("#change-color button");
@@ -33,22 +76,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // Update wallpaper image based on the clicked button
             const imgSrc = button.querySelector("img").getAttribute("src");
-
             wallpaper.setAttribute("src", imgSrc);
         });
     });
 
-    const powerButton = document.getElementById("power");
-    const pill = document.getElementById("pill");
-    const statusBar = document.getElementById("status-bar");
-    const dynIsland = document.getElementById("dyn-island");
-    const dynIslandAOD = document.getElementById("dyn-island-aod");
-    const muteButton = document.getElementById("mute");
-    const dynIslandSilent = document.getElementById("dyn-island-silent");
-    const controlCenter = document.getElementById("control-center");
-    const signal = document.getElementById("signal");
-    const dynIslandRing = document.getElementById("dyn-island-ring");
 
+    // add event click listner to power button
     powerButton.addEventListener("click", () => {
         if (pill.classList.contains("bg-white") && !statusBar.classList.contains("hidden")) {
             pill.classList.remove("bg-white");
@@ -84,6 +117,7 @@ document.addEventListener("DOMContentLoaded", function() {
         return null;
     }
 
+    // adds event click listner to mute button
     muteButton.addEventListener("click", () => {
         // Check if 'muted' cookie is present
         const mutedCookieValue = getMutedCookie();
@@ -99,6 +133,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 signal.classList.add("hidden");
             }
 
+            // wait 3.5s before hiding banner
             setTimeout(() => {
                 console.log("muting");
                 dynIslandSilent.classList.add("hidden");
@@ -125,6 +160,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 signal.classList.add("hidden");
             }
 
+            // wait 3.5s before hiding banner
             setTimeout(() => {
                 console.log("unmuting");
                 dynIslandRing.classList.add("hidden");
@@ -145,44 +181,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
     })
 
-    const player = document.getElementById('player');
-    const cover = document.getElementById('cover');
-    const trackName = document.getElementById('title');
-    const artistName = document.getElementById('artist');
-    const elapsed = document.getElementById('elapsed');
-    const length = document.getElementById('length');
-    const scrubber = document.getElementById('scrubber');
-    const prevButton = document.getElementById('prev');
-    const playButton = document.getElementById('playpause');
-    const nextButton = document.getElementById('next');
-    const playIcon = document.getElementById('play');
-    const pauseIcon = document.getElementById('pause');
+    // spotify and music player controls
 
-    const clientId = '5f56ac4110084091bcd953a152dd05a7';
-    const redirectUri = 'http://127.0.0.1:5501/src/animation3.html';
-    const scopes = ['user-read-playback-state', 'user-read-currently-playing', 'user-modify-playback-state'];
-
+    // url to authorize spotify and get access token
     var url = 'https://accounts.spotify.com/authorize';
     url += '?response_type=token';
     url += '&client_id=' + encodeURIComponent(clientId);
     url += '&scope=' + encodeURIComponent(scopes);
     url += '&redirect_uri=' + encodeURIComponent(redirectUri);
 
+    // check the url and gets the token every 1 hr
     if (window.location.href.includes('access_token')) {
         setTimeout(() => {
             window.location.href = url;
         }, 3600000)
-    } else {
-        window.location.href = url;
     }
 
-    console.log(url);
-    let accessToken;
+    // redirect to spotify for authentication
+    spotifyBtn.addEventListener('click', () => {
+        if (window.location.href.includes('access_token')) {
+            window.history.replaceState({}, document.title, window.location.href.split("#")[0]);
+        } else {
+            window.location.href = url;
+        }
+    })
+
     // Check if the URL contains an access_token parameter
+    let accessToken;
     if (window.location.href.includes('access_token')) {
         accessToken = window.location.href.split('#')[1].split('=')[1].split('&')[0];
     }
-    console.log(accessToken);
+    // console.log(accessToken);
 
     function convertMsToMinsAndSecs(milliseconds, is_negative) {
         const totalSeconds = Math.floor(milliseconds / 1000);
@@ -203,18 +232,24 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .then(response => response.json())
         .then(data => {
-            const currentTrack = data.item;
+            const jsonData = JSON.parse(data);
+            if (!data.item) {
+                console.log('No track currently playing.');
+                return;
+            }
+            const currentTrack = jsonData.item;
             cover.src = currentTrack.album.images[0].url;
             trackName.textContent = currentTrack.name;
             artistName.textContent = currentTrack.artists.map(artist => artist.name).join(', ');
-            const progress = data.progress_ms;
+            const progress = jsonData.progress_ms;
             const duration = currentTrack.duration_ms;
             const remaining = duration - progress;
             length.textContent = convertMsToMinsAndSecs(remaining, true);
             elapsed.textContent = convertMsToMinsAndSecs(progress, false);
             const percentage = Math.round((progress / duration) * 100);
             scrubber.style.width = `${percentage}%`;
-            if (!data.is_playing || false) {
+            const playing = jsonData.is_playing === true ? true : false;
+            if (!playing) {
                 playIcon.classList.remove("hidden");
                 pauseIcon.classList.add("hidden");
                 setTimeout(() => {
@@ -231,38 +266,8 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    setInterval(() => {
-        fetchCurrentTrack();
-    }, 1000);
-
     function playPauseTrack() {
-        console.log(currentTrack.duration_ms);
-        // const url = 'https://api.spotify.com/v1/me/player/play';
-        // const headers = {
-        //     'Authorization': `Bearer ${accessToken}`,
-        //     'Content-Type': 'application/json'
-        // };
-
-        // const data = {
-        //     context_uri: 'spotify:album:5ht7ItJgpBH7W6vJ5BqpPr',
-        //     offset: {
-        //         position: 5
-        //     },
-        //     position_ms: 0
-        // };
-
-        // fetch(url, {
-        //     method: 'PUT',
-        //     headers: headers,
-        //     body: JSON.stringify(data)
-        // })
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log(data); // Response from the Spotify API
-        // })
-        // .catch(error => {
-        //     console.error('Error:', error);
-        // });
+        // TODO: //
     }
     
     // Play Next Track
@@ -294,4 +299,53 @@ document.addEventListener("DOMContentLoaded", function() {
     playButton.addEventListener('click', playPauseTrack);
     nextButton.addEventListener('click', playNextTrack);
     prevButton.addEventListener('click', playPreviousTrack);
+
+    function getBatteryLevel() {
+        navigator.getBattery()
+            .then((battery) => {
+                const level = battery.level * 100;
+                const isCharging = battery.charging;
+                batteryPer.innerHTML = isCharging && level != 100 ? level + chargingSvg : level;
+                batteryIndicator.style.width = `${level}%`;
+                if (isCharging) {
+                    batteryIndicator.classList.add('bg-green-400');
+                    batteryPer.classList.remove('text-slate-600');
+                    batteryPer.classList.remove('left-[325px]');
+                    if (level === 100) {
+                        batteryTip.classList.remove('bg-gray-400/90');
+                        batteryTip.classList.add('bg-green-400');
+                    } else {
+                        batteryPer.classList.add('left-[323px]');
+                        batteryPer.classList.add('text-white');
+                    }
+                    // batteryTip.classList.add('bg-green-400');
+                } else {
+                    batteryIndicator.classList.remove('bg-green-400');
+                    batteryPer.classList.add('text-slate-600');
+                    batteryPer.classList.add('left-[325px]');
+                    if (level === 100) {
+                        batteryTip.classList.add('bg-gray-400/90');
+                        batteryTip.classList.remove('bg-green-400');
+                    } else {
+                        batteryPer.classList.remove('left-[323px]');
+                        batteryPer.classList.remove('text-white');
+                    }
+                }
+                if (level === 100) {
+                    batteryPer.classList.add('left-[321px]');
+                    batteryIndicator.classList.add('rounded-r-[5px]');
+                } else {
+                    batteryPer.classList.remove('left-[321px]');
+                    batteryIndicator.classList.remove('rounded-r-[5px]');
+                }
+
+            })
+    }
+
+    setInterval(() => {
+        if (window.location.href.includes('access_token')) {
+            fetchCurrentTrack();
+        }
+        getBatteryLevel();
+    }, 1000);
 })
